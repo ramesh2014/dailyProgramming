38a477e8050019edb88cbd1cd12eda06532b4610
ramnagar <ramnagar@192.168.0.134>
2019-12-01 21:53:49 +0530

    First commit

---
+++ SamplePrograms/daily/samples/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package daily.samples;
+
+/**
+ * @author ramnagar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+
+}
---
+++ SamplePrograms/daily/samples/UnRepeatedNumber.java
@@ -0,0 +1,18 @@
+package daily.samples;
+
+public class UnRepeatedNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[] = {1,3,2,5,4,3,4,2,5};
+		
+		int i, res = 0;
+		
+		for(i=0;i<a.length;i++) {
+			res = res^a[i];
+		}
+		System.out.println(res);
+	}
+
+}
