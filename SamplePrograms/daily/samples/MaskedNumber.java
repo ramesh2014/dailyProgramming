c81c020fe30026da8e65253b5c09abf407859b7f
ramnagar <ramnagar@192.168.0.134>
2019-12-26 22:15:53 +0530

    Test

--- SamplePrograms/daily/samples/MaskedNumber.java
+++ SamplePrograms/daily/samples/MaskedNumber.java
@@ -16,19 +16,26 @@ public class MaskedNumber {
         
         //AMEX - is for CreditCardSetup, CARD_BRAND_AMEX - is for while uploading cc through feed
         Boolean bIsCardBrandAmex = (!(null == cardBrand) || (! "".equals(cardBrand))) && ("AmericanExpress".equals(cardBrand) || "AMEX".equals(cardBrand));
-        String maskedCCNumber = null;
+        String maskedCCNumber = "XXXXXXXXXXXXXXXX";
         String maskedCharAmex = "XXXXXXXXXXXX";
         String maskedCharCDF3 = "XXXXXXXXXXXX";
-        String strippedNumber = stripNumberOfChars(cardNumber);
+        Boolean bIsCardNumber = (!(null == cardNumber) && (! "".equals(cardNumber)));
+        String strippedNumber = null;
+        int length = 0;
         
-        int length = strippedNumber.length();
+        if(bIsCardNumber) {
+        	strippedNumber = stripNumberOfChars(cardNumber);
         
-        if (bIsCardBrandAmex) 
-            maskedCCNumber = (length >= 4 ? maskedCharAmex + strippedNumber.substring(length - 4) : 
-            	(maskedCharAmex + maskedCharAmex.substring(maskedCharAmex.length()-4).substring(length) + strippedNumber));
-        else
-        	maskedCCNumber = (length >= 4 ? maskedCharCDF3 + strippedNumber.substring(length - 4) : 
-            	(maskedCharCDF3 + maskedCharCDF3.substring(maskedCharCDF3.length()-4).substring(length) + strippedNumber));
+	        length = strippedNumber.length();
+	        
+	        if (bIsCardBrandAmex) 
+	            maskedCCNumber = (length >= 4 ? maskedCharAmex + strippedNumber.substring(length - 4) : 
+	            	(maskedCharAmex + maskedCharAmex.substring(maskedCharAmex.length()-4).substring(length) + strippedNumber));
+	        else
+	        	maskedCCNumber = (length >= 4 ? maskedCharCDF3 + strippedNumber.substring(length - 4) : 
+	            	(maskedCharCDF3 + maskedCharCDF3.substring(maskedCharCDF3.length()-4).substring(length) + strippedNumber));
+        }
+        
         return maskedCCNumber;
     }
     
@@ -40,12 +47,16 @@ public class MaskedNumber {
 		finalValue = getMaskedCardNumber("AMEX","456");
 		System.out.println(finalValue);
 		
-		finalValue = getMaskedCardNumber("AMEX","1");
+		finalValue = getMaskedCardNumber("AMEX", "1");
 		System.out.println(finalValue);
 		
 		finalValue = getMaskedCardNumber("AMEX","12");
 		System.out.println(finalValue);
 		
+		finalValue = getMaskedCardNumber("AMEX","");
+		System.out.println(finalValue);
 		
+		finalValue = getMaskedCardNumber("AMEX",null);
+		System.out.println(finalValue);
 	}
 }
